1e2a4bbb1956d729fba7568a24adf8f8434c8244
君莫笑 <1768151377@qq.com>
2024-11-25 14:38:30 +0800

    Update FileReader.cs
    添加引用库

--- LasLib.net/Reader/FileReader.cs
+++ LasLib.net/Reader/FileReader.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
